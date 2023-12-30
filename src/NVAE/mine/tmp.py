9ab61e3031d209c0487ec9c3272afb02482256ff
dserez <serez1997@gmail.com>
2023-12-30 12:31:52 +0100

    discretiedlogisticmixture

--- src/NVAE/mine/tmp.py
+++ src/NVAE/mine/tmp.py
@@ -1,4 +1,6 @@
 # code adapted from https://github.com/mgp123/nvae/blob/main/model/distributions.py
+# and from https://github.com/openai/pixel-cnn/blob/master/pixel_cnn_pp/nn.py
+
 # to understand the Mixture of Logistics, check https://github.com/Rayhane-mamah/Tacotron-2/issues/155
 import torch
 from einops import rearrange, repeat
@@ -78,7 +80,7 @@ class DiscMixLogistic:
         # expand with num mixtures
         samples = repeat(samples, 'b c h w -> b n (c h w)', n=self.num_mixtures)
 
-        # compute CDF
+        # compute CDF in the neighborhood of each sample
         centered = samples - self.means_logits
         neg_scale = torch.exp(- self.log_scales_logits)
 
@@ -88,26 +90,24 @@ class DiscMixLogistic:
         min_in = neg_scale * (centered - 1. / self.max_val)
         cdf_min = torch.sigmoid(min_in)
 
-        # CDF in the neighborhood of each sample
-        cdf_delta = cdf_plus - cdf_min
+        # log probability for edge case of 0
+        log_cdf_plus = plus_in - torch.nn.functional.softplus(plus_in)
 
-        # Special cases (around black and white pixel values)
-        log_cdf_plus = plus_in - F.softplus(plus_in)
-        log_one_minus_cdf_min = - F.softplus(min_in)
+        # log probability for edge case of 255
+        log_one_minus_cdf_min = - torch.nn.functional.softplus(min_in)
 
-        mid_in = neg_scale * centered
-        log_pdf_mid = mid_in - self.log_scales_logits - 2. * F.softplus(mid_in)
+        # probability for all other cases (avoid very low values)
+        cdf_delta = torch.clamp(cdf_plus - cdf_min, 1e-10)
 
-        log_prob_mid_safe = torch.where(cdf_delta > 1e-5,
-                                        torch.log(torch.clamp(cdf_delta, min=1e-10)),
-                                        log_pdf_mid - np.log(self.max_val / 2))
+        # select the right output: left edge case, right edge case, normal case
+        # simplified version w.r.t original (TF implementation)
+        # for very low probs, here we simply clamp values to 1e-10
+        log_probs = torch.log(cdf_delta)
+        log_probs = torch.where(samples < -0.999, log_cdf_plus, log_probs)
+        log_probs = torch.where(samples > 0.99, log_one_minus_cdf_min, log_probs)
 
-        # the original implementation uses samples > 0.999, this ignores the largest possible pixel value (255)
-        # which is mapped to 0.9922
-        log_probs = torch.where(samples < -0.999, log_cdf_plus,
-                                torch.where(samples > 0.99, log_one_minus_cdf_min, log_prob_mid_safe))
-
-        log_probs = torch.sum(log_probs, 1) + F.log_softmax(self.logistic_logits, dim=1)
+        # log_probs is B N (C H W)
+        log_probs = log_probs + torch.nn.functional.log_softmax(self.logistic_logits, dim=1)
         return torch.logsumexp(log_probs, dim=1)
 
     def sample(self):
@@ -150,7 +150,8 @@ if __name__ == '__main__':
 
     # training.
     recon = disc_mixture.log_prob(gt)
-    # loss = - torch.sum(recon, dim=[1, 2])  # summation over RGB is done.
+    loss = - torch.sum(recon, dim=1)  # summation over RGB is done.
+    print(loss)
 
     # sampling.
     # img = disc_mixture.sample()
